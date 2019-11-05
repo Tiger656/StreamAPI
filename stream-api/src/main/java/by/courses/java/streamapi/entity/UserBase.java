bb9e75acb03211d1acbb7025f935e602b8fcbed7
Tim <nbvjatqswat@gmail.com>
2019-11-05 23:24:24 +0300

    fix: fix "RemoveAllOrder", "IsCharacterPresentInAllNames"

--- stream-api/src/main/java/by/courses/java/streamapi/entity/UserBase.java
+++ stream-api/src/main/java/by/courses/java/streamapi/entity/UserBase.java
@@ -3,11 +3,26 @@ package by.courses.java.streamapi.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor(staticName = "of")
 public class UserBase {
 
     private String name;
 
-    private int age;
+    private Integer age;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserBase person = (UserBase) o;
+        return age.equals(person.age) &&
+                name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
 }
--- stream-api/src/main/java/by/courses/java/streamapi/operation/DefaultStream.java
+++ stream-api/src/main/java/by/courses/java/streamapi/operation/DefaultStream.java
@@ -3,48 +3,49 @@ package by.courses.java.streamapi.operation;
 import by.courses.java.streamapi.entity.UserBase;
 
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class DefaultStream implements Operation<UserBase> {
 
     @Override
     public Collection<UserBase> removeWithMaxAge(Collection<UserBase> entities) {
-        return null;
+        return entities.stream().filter(user -> user.getAge() < (entities.stream().map(UserBase::getAge).mapToInt(v -> v).max().getAsInt())).collect(Collectors.toList());
     }
 
     @Override
     public Collection<UserBase> removeAllOlder(Collection<UserBase> entities, int age) {
-        return null;
+        return entities.stream().filter(user -> user.getAge() <= age).collect(Collectors.toList());
     }
 
     @Override
     public double getAverageAge(Collection<UserBase> entities) {
-        return 0;
+        return entities.stream().map(UserBase::getAge).mapToInt(v -> v).average().getAsDouble();
     }
 
     @Override
     public UserBase getThirdInCollection(Collection<UserBase> entities) {
-        return null;
+        return entities.stream().skip(2).findFirst().get();
     }
 
     @Override
     public Collection<UserBase> getTwoUsersStartingFromSecond(Collection<UserBase> entities) {
-        return null;
+        return entities.stream().skip(1).limit(2).collect(Collectors.toList());
     }
 
     @Override
     public boolean isCharacterPresentInAllNames(Collection<UserBase> entities, String character) {
-        return false;
+        return entities.stream().allMatch(person -> person.getName().toLowerCase().contains(character.toLowerCase()));
     }
 
     @Override
     public Collection<UserBase> addValueToAllNames(Collection<UserBase> entities, String value) {
-        return null;
+        return entities.stream().peek(person -> person.setName(person.getName() + value)).collect(Collectors.toList());
     }
 
     @Override
     public Collection<UserBase> sortByNameThanByAge(Collection<UserBase> entities) {
-        return null;
+        return entities.stream().sorted((p1, p2) ->
+                p1.getName() != p2.getName() ? p1.getName().compareTo(p2.getName()) :
+                        p1.getAge().compareTo(p2.getAge())).collect(Collectors.toList());
     }
 }
